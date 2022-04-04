fa05dc9da91720273bb1585559678194780f6fb0
ARKOMOL <80365662+ARKOMOL@users.noreply.github.com>
2022-04-04 22:30:18 +0600

    rest chart done

--- src/Component/Dashboard/DashBoard.js
+++ src/Component/Dashboard/DashBoard.js
@@ -1,6 +1,8 @@
 
 import React from 'react';
+import AreaCharts from '../Charts/AreaChart';
 import BraChart from '../Charts/BarChart';
+import LineBarAreaChart from '../Charts/LineBarArea';
 import LineCharts from '../Charts/LineChart';
 
 const DashBoard = () => {
@@ -8,7 +10,8 @@ const DashBoard = () => {
         <div className=' sm:grid-cols-1 lg:grid lg:grid-cols-2  flex-2 w-100'>
             <BraChart/>
             <LineCharts/>
-          
+            <AreaCharts/>
+            <LineBarAreaChart/>
             
         </div>
     );
